7c5aea158a332001e668c6dcb29ea9ed133188ec
Envisage <envisage256@gmail.com>
2025-03-07 15:11:58 +0300

    Site Upload

---
+++ JS/api.js
@@ -0,0 +1,5 @@
+var recyclingCenters = [
+    { lat: 51.5074, lng: -0.1278, name: "Center Name" },
+    // Add other recycling centers...
+  ];
+  
---
+++ JS/sidenav.js
@@ -0,0 +1,14 @@
+// function openNav() {
+//     document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidenav").style.width = "0";
+// }
+
+
+
+function toggleNav() {
+    document.getElementById("sideNav").classList.toggle("open")
+}
+
